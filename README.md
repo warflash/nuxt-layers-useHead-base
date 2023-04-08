7f110b360d7f13ab7406b7fc0c3ffac2e97b9cf2
warflash <nwiesinger67@gmail.com>
2023-04-09 00:53:54 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# nuxt-layers-useHead-base
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/github-3egbdt-wtqgbp)
---
+++ pages/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>Base Page Index</div>
+</template>
+
+<script setup lang="ts">
+useHead({
+  title: 'Nuxt Base',
+  meta: [
+    {
+      name: 'description',
+      content: 'Nuxt Base!',
+    },
+  ],
+});
+</script>
